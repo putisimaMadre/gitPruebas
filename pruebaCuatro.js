9cef97af9b12656e70596a7423739323a2251782
putisima_madre <putisima_madre@hotmail.com>
2023-05-24 09:49:55 -0600

    se añaden varias cosas del sat

---
+++ pruebaCuatro.js
@@ -0,0 +1,21 @@
+//esto es un for
+const marcas = ['sony', 'sanyo']
+marcas.forEach((valor)=>{
+    console.log(valor)
+})
+
+
+declaracion anual
+fiel constrasena
+constancia de situacion fiscal actualizada
+
+comprobante de domicilio
+
+declaracion anual
+
+16 enero 
+
+francisco 
+
+
+else.firma y constrasena
